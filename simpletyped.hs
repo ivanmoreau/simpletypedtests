69eb9802f64b3538d2768ec7e10dd5f5173087d1
Ivan Molina Rebolledo <ivanmolinarebolledo@gmail.com>
2022-04-10 04:42:40 -0500

    Changes.

--- simpletyped.hs
+++ simpletyped.hs
@@ -14,34 +14,36 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 {-# LANGUAGE DataKinds, GADTSyntax, TypeFamilies, TypeOperators  #-}
 
-
+import Type.Reflection
+import Unsafe.Coerce (unsafeCoerce)
 
 
 
 
 data Expr a where
+  Val :: a -> Expr a
   Var :: Int -> Expr a
   Lib :: String -> Expr a
   Lam :: String -> Expr α -> Expr (a -> β)
-  App :: Expr (α -> β) -> Expr α -> Expr β
+  Lpp :: Expr (α -> β) -> Expr α -> Expr β
+  deriving Typeable
 
-data Definition a where
-  Def :: String -> Expr a -> Definition a
+data Definition α where
+  Def :: forall α. Typeable α => String -> Expr α -> Definition α
+
+data Definable where
+  Definable :: Typeable α => Definition α -> Definable
 
 data TypeSingleton a where
   TS :: a -> TypeSingleton a
   Holder :: TypeSingleton a
-
--- Intencional equality
-data (≡) a b where
-  Refl :: a ≡ a
 
 (↑) :: Int -> Int -> Expr α -> Expr α
 (d ↑ c) (Var n)
   | c <= n = Var (n - 1)
   | otherwise = Var n
 (d ↑ c) (Lib n) = Lib n
-(d ↑ c) (App t₀ t₁) = App ((d ↑ c) t₀) ((d ↑ c) t₁)
+(d ↑ c) (Lpp t₀ t₁) = Lpp ((d ↑ c) t₀) ((d ↑ c) t₁)
 (d ↑ c) (Lam τ t) = Lam τ $ ((d ↑) $ c + 1) t
 
 (↓) :: Int -> Int -> Expr α -> Expr α
@@ -49,22 +51,52 @@ data (≡) a b where
   | c <= n = Var (n - 1)
   | otherwise = Var n
 (d ↓ c) (Lib n) = Lib n
-(d ↓ c) (App t₀ t₁) = App ((d ↓ c) t₀) ((d ↓ c) t₁)
+(d ↓ c) (Lpp t₀ t₁) = Lpp ((d ↓ c) t₀) ((d ↓ c) t₁)
 (d ↓ c) (Lam var t) = Lam var $ ((d ↓) $ c - 1) t
 
-ts2Holder :: TypeSingleton a -> TypeSingleton a
-ts2Holder (TS a) = Holder
+-- Get empty type from Expr
+getExprT :: Expr a -> a
+getExprT e = unsafeCoerce e
 
-expr2Holder :: Expr a -> TypeSingleton a
-expr2Holder (Var _) = Holder
+-- Get empty type from TypeSingleton
+getTS :: TypeSingleton a -> a
+getTS (TS a) = a
+
+-- Is the same type as the one in the definition
+isType :: (Typeable α, Typeable β, α ~ β) => TypeSingleton α -> Expr β -> Maybe (α :~~: β)
+isType f s =
+  let a = typeOf (getTS f)
+      b = typeOf (getExprT s) in
+  eqTypeRep a b
+
 
 lookupDefinition :: String -> TypeSingleton α -> [Definition α] -> Maybe (Expr α)
-lookupDefinition var (TS t) [] = Nothing
-lookupDefinition var (TS t) ((Def var' t'):ds)
-  | var == var' = let et = expr2Holder t' in
-    if et == Holder then Just t' else Nothing
-  | otherwise = lookupDefinition var (TS t) ds
+lookupDefinition var _ [] = Nothing
+lookupDefinition var t ((Def var' t'):ds)
+  | var == var' = let res = isType t t' in
+    case res of
+      Just HRefl -> Just t'
+      Nothing -> lookupDefinition var t ds
+  | otherwise = lookupDefinition var t ds
 
+
+------ Test cases ------
+
+-- List of definitions
+
+builderDef :: Typeable α => String -> Expr α -> Definable
+builderDef a b = Definable $ Def a b
+
+defs :: [Definable]
+defs = [
+  builderDef "false" $ Val False,
+  builderDef "true" $ Val True,
+  builderDef "zero" $ Val (0 :: Int)
+  ]
+
+infere = Lam "n" $ Val False
+
+test = Fun (typeOf (1 :: Int)) (typeOf True)
 
 
 
